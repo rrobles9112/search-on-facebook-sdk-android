8cde0240b6524e56858c77ee66e80393e6d2ab0c
richard robles <rrobles9112@gmail.com>
2018-11-29 09:15:36 -0500

    Created example host another module

---
+++ app_host/src/main/java/com/example/app_host/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.app_host
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.btn_id_gosearch)
+
+        btn.setOnClickListener {
+
+        }
+
+    }
+
+
+}
